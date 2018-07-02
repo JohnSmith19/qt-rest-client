0b71cafe6fcf34674d4cc2c579e610b32bfa29ce
John Smith <unknown.prop@gmail.com>
2018-07-02 20:03:32 +0900

    UI 초기화

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,18 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QString>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    ui->paramTable->setColumnCount(2);
+    ui->paramTable->setHorizontalHeaderLabels(QString("key;value").split(";"));
+    ui->paramTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    ui->urlEdit->setText("http://127.0.0.1:5000/");
 }
 
 Widget::~Widget()
